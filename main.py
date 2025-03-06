837fb1e3140a7afefa4af6f631996948748fc529
LunarTides <31688109+LunarTides@users.noreply.github.com>
2025-03-06 12:33:37 +0100

    Improve `main.py`

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 from rich import print
 
-import cards
 from src.card import Card
 from src.commands import CommandError
 from src.game import create_game
 
 game = create_game()
 
-# Assert that the current player is not the next player
-assert game.current_player != game.next_player, (
-    "Current player is the next player. Something has gone very wrong."
-)
-
-# Two ways to import a card.
 # TODO: Remove
-sheep1 = cards.sheep1.copy(game.current_player)
+sheep1 = Card.from_unique_id(1).copy(game.current_player)
 sheep2 = Card.from_unique_id(1).copy(game.next_player)
 
 # I know :)
@@ -24,44 +17,44 @@ game.current_player.add_to_hand(sheep1)
 game.next_player.add_to_hand(sheep2)
 
 while game.running:
-    # Clear screen.
     game.interact.clear_screen()
 
     print(game.interact.watermark())
     print()
-    print(game.player1.visualize_board())
-    print(game.player2.visualize_board(True))
+    print(game.player1.visualize_board(add_separator=False))
+    print(game.player2.visualize_board(add_separator=True))
     print()
     print(game.current_player.visualize_hand())
 
     try:
         user = input("\nWhich card do you want to play? ")
+
+        # TODO: Remove
+        if user == "q":
+            game.running = False
+            break
+
+        if user.isnumeric():
+            user = int(user)
+            if user < 1 or user > len(game.current_player.hand):
+                print("[red]Invalid card[/red]")
+                input()
+                continue
+
+            card = game.current_player.hand[user - 1]
+            game.current_player.play_card(card)
+            continue
+
+        # Parse the command.
+        cmd = user.split(" ")[0]
+        args = user.split(" ")[1:]
+
+        try:
+            game.commands.handle(cmd, args)
+        except CommandError as e:
+            print(f"[red]{e.message}[/red]")
+            input()
+            continue
     except KeyboardInterrupt:
         game.running = False
         break
-
-    if user == "q":
-        game.running = False
-        break
-
-    if user.isnumeric():
-        user = int(user)
-        if user > len(game.current_player.hand):
-            print("[red]Invalid card[/red]")
-            input()
-            continue
-
-        card = game.current_player.hand[user - 1]
-        game.current_player.play_card(card)
-        continue
-
-    # Parse the command.
-    cmd = user.split(" ")[0]
-    args = user.split(" ")[1:]
-
-    try:
-        game.commands.handle(cmd, args)
-    except CommandError as e:
-        print(f"[red]{e.message}[/red]")
-        input()
-        continue
